eb9036e8ae184dee47dc0cfbebd5dc6e47a45e5d
unknown <98nba@naver.com>
2021-01-07 16:50:10 +0900

    test

--- assets/js/disqus_comments.js
+++ assets/js/disqus_comments.js
@@ -2,8 +2,8 @@
   var disqus_config;
 
   disqus_config = function() {
-    this.page.url = 'http://teat-account.github.io/assets/js/disqus_comments.js';
-    this.page.identifier = 'http://teat-account.github.io/assets/js/disqus_comments.js';
+    this.page.url = 'https://teat-account.github.io/assets/js/disqus_comments.js';
+    this.page.identifier = 'https://teat-account.github.io/assets/js/disqus_comments.js';
     return false;
   };
 
